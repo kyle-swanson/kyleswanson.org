8441be82e167e054078a87d247d203179ad90bf0
Kyle Swanson <ext.kswanson@riotgames.com>
2018-04-20 13:27:59 -0600

    Netlify test

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,7 @@ import Typography from 'material-ui/Typography'
 import Paper from 'material-ui/Paper'
 import Button from 'material-ui/Button'
 import Grid from 'material-ui/Grid'
+import Chip from 'material-ui/Chip';
 
 import profileImage from '../images/profile.jpg'
 import scrollerImage from '../../images/reactVizGif.gif'
@@ -17,6 +18,33 @@ const imgStyle = {
   display: 'block',
   width: '100%',
 }
+
+const LINKS_LIST = [
+  {
+    l: 'GitHub',
+    a: 'https://github.com/skierkyles',
+  },
+  {
+    l: 'LinkedIn',
+    a: 'https://www.linkedin.com/in/kyle-swanson-48b70a105',
+  },
+  {
+    l: 'Facebook',
+    a: 'https://www.facebook.com/kyle.swanson.91',
+  },
+  {
+    l: 'Instagram',
+    a: 'https://www.instagram.com/skierkyles/',
+  },
+  {
+    l: 'Strava',
+    a: 'https://www.strava.com/athletes/486083',
+  },
+  {
+    l: 'Universe',
+    a: 'https://universe.leagueoflegends.com/en_US/',
+  },
+]
 
 export default withSiteData(() => (
   <div style={containerStyle}>
@@ -41,7 +69,7 @@ export default withSiteData(() => (
 
       <Grid item xs={12}>
         <Typography variant="body1">
-          I like working with: Python (Django), Javascript (React, Angular, Webpack),
+          Right now I'm working with: Python (Django), Javascript (React, Angular, Webpack, etc),
           SCSS, HTML, CSS, Java, etc.
         </Typography>
       </Grid>
@@ -52,8 +80,8 @@ export default withSiteData(() => (
           extensivly with the built in Django templating/view system. Built hybrid
           mobile apps using Angular and the Ionic framework, and even a native Java
           Android app. I've used websockets for quick communication between different
-          mobile clients in a head to head game. Currently I'm most excited building
-          ReactJS applications (Believe the hype).
+          mobile clients in a head to head game. Currently I'm building
+          ReactJS applications (It's amazing believe the hype!).
         </Typography>
       </Grid>
 
@@ -86,7 +114,7 @@ export default withSiteData(() => (
           Links
         </Typography>
 
-
+        { LINKS_LIST.map(({ l, a }) => <Chip style={{ marginRight: 8, cursor: 'pointer' }} key={a} component="a" href={a} label={l} />) }
       </Grid>
     </Grid>
   </div>
